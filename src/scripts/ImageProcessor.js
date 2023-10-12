63f8ae7aaa000d1a09e883f2cbe859cef07d3a5e
pancake423 <58053103+pancake423@users.noreply.github.com>
2023-10-12 15:04:43 -0400

    initial processing steps
    added the first few steps of cube processing-
    Scaling down image
    Extracting cube from background
    
    note: to simplify problem, all images will be taken of a single face on a black background (for now).

--- src/scripts/ImageProcessor.js
+++ src/scripts/ImageProcessor.js
@@ -7,30 +7,77 @@ ImageProcessor.process = (data) => {
 
 //scales an image to be targetWidth pixels wide.
 ImageProcessor.scale = (data, targetWidth) => {
+	const DENSITY = 0.1;
+
 	const compressionRatio = data.width / targetWidth;
 
 	const out = new ImageData(targetWidth, Math.floor(data.height) / compressionRatio);
-
+	for (let x = 0; x < out.width; x++) {
+		for (let y = 0; y < out.height; y++) {
+			let [r, g, b, a] = ImageProcessor.getAverageColor(
+				data, 
+				Math.floor(x * compressionRatio),
+				Math.floor(y * compressionRatio),
+				Math.ceil(compressionRatio),
+				Math.ceil(compressionRatio),
+				DENSITY
+			);
+			let idx = (x + y * out.width)*4;
+			out.data[idx] = r;
+			out.data[idx + 1] = g;
+			out.data[idx + 2] = b;
+			out.data[idx + 3] = a;
+		}
+	}
 
 	return out;
 }
 
 //returns the [r, g, b, a] values of a given pixel.
 ImageProcessor.getPixel = (data, x, y) => {
-	return data.slice((x + y * data.width) * 4, (x + y * data.width) * 4 + 4);
+	return data.data.slice((x + y * data.width) * 4, (x + y * data.width) * 4 + 4);
+}
+
+ImageProcessor.setPixel = (data, x, y, color) => {
+	startIndex = (x + y * data.width) * 4;
+	color.forEach((c, i) => data.data[startIndex + i] = c);
+}
+
+ImageProcessor.copy = (data) => {
+	let dataCopy = new Uint8ClampedArray(data.data);
+	let out = new ImageData(dataCopy, data.width, data.height);
+	return out;
 }
 
 //returns the average channel values of a given region of an image
-imageProcessor.getAverageColor = (data, x, y, dx, dy) => {
-	const totals = [0, 0, 0, 0];
-	const area = dx * dy;
+ImageProcessor.getAverageColor = (data, x, y, dx, dy, sampleDensity) => {
+	let totals = [0, 0, 0, 0];
 
-	for (let xpos = x; xpos < x + dx; xpos++) {
-		for (let ypos = y; ypos < y + dy; ypos++) {
-			const p = ImageProcessor.getPixel(data, xpos, ypos);
-			totals = totals.map((v, i) => v + p[i]);
-		}
+	const nSamples = Math.ceil(dx * dy * sampleDensity);
+
+	for (let i = 0; i < nSamples; i++) {
+		const xpos = Math.floor(Math.random() * dx) + x;
+		const ypos = Math.floor(Math.random() * dy) + y;
+		const p = ImageProcessor.getPixel(data, xpos, ypos);
+		totals = totals.map((v, i) => v + p[i]);
 	}
 
-	return totals.map((v) => Math.floor(v /= area));
+	return totals.map((v) => Math.floor(v /= nSamples));
+}
+ImageProcessor.filterDarkRegions = (data, brightnessThreshold) => {
+	/*
+	set color of all pixels below the brightness threshold to [0, 0, 0, 0].
+	brightness threshold is max(r, g, b).
+	*/
+	const out = ImageProcessor.copy(data);
+
+	for (let x = 0; x < data.width; x++) {
+		for (let y = 0; y < data.height; y++) {
+			const brightness = Math.max(...ImageProcessor.getPixel(data, x, y).slice(0, 3));
+			if (brightness < brightnessThreshold) {
+				ImageProcessor.setPixel(out, x, y, [0, 0, 0, 0]);
+			}
+		}
+	}
+	return out;
 }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -15,7 +15,7 @@ window.onload = () => {
 
 function handleFileUpload(e) {
 	blobToImageData(e.srcElement.files[0])
-	.then((data) => console.log(data))
+	.then((data) => demo(data));
 }
 
 /*
@@ -32,4 +32,43 @@ function blobToImageData(blob) {
 
 		return ctx.getImageData(0, 0, bitmap.width, bitmap.height);
 	});
+}
+
+/*
+adds a canvas to the body and draws the provided ImageData to it.
+*/
+function showImageData(data) {
+	const c = document.createElement("canvas");
+	c.width = data.width;
+	c.height = data.height;
+	c.className = "img-display";
+	document.body.appendChild(c);
+	const ctx = c.getContext("2d");
+	createImageBitmap(data)
+		.then(img => ctx.drawImage(img, 0, 0));
+
+}
+
+/*
+takes an ImageData object and runs it through the cube detection process.
+*/
+function demo(data) {
+	const SCALE_PX = 80;
+	const DARK_THRESH = 80;
+	const original = data;
+	const scaled = ImageProcessor.scale(original, SCALE_PX);
+	const noBg = ImageProcessor.filterDarkRegions(scaled, DARK_THRESH);
+
+	console.log(original, scaled, noBg);
+
+	showImageData(original);
+	showImageData(scaled);
+	showImageData(noBg);
+
+	// crop image to only include mask.
+	// somehow figure out rotation angle.
+	// compensate for rotation.
+	// divide image into 3x3 grid
+	// find avg color of each tile
+	// use KNN model + training data to match colors to letters
 }
